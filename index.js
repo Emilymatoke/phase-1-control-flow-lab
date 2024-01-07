0d692b48361789ace74c99a4ce2bb7e15f9291d1
Emilymatoke <emilymomata@gmail.com>
2024-01-07 09:10:03 -0500

    Assignment Completed.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+let  scuberGreetingForFeet(199), scuberGreetingForFeet(2001), scuberGreetingForFeet(2501);
+
+  if(scuberGreetingForFeet <= 400){
+    scuberGreetingForFeet(199)= 'This is on me!'
+  }else if (scuberGreetingForFeet >2000) {
+     scuberGreetingForFeet(2001)='I will gladly take your thirty bucks.'
+  } else if (scuberGreetingForFeet > 2500) {
+      scuberGreetingForFeet(2501) = 'No can do.'
+  }
+
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  const city = NYC;
+    city = NYC ? 'Ok, sounds good.': 'No go.'
+
+
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(switchOnCharmFromTip) {
+     case 'generous':
+      return  'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+      default:
+        return 'Bye'
+        break;
+
+  
+}
 }
